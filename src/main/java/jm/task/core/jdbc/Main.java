63bbd337c3948d2c53d976d64eb3d62060371aca
Maxim Glebov <maglebov@gmail.com>
2024-04-20 07:14:17 +0300

    delete files

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,6 +2,7 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
@@ -10,11 +11,12 @@ import java.util.List;
 
 public class  Main {
     public static void main(String[] args) {
-        UserServiceImpl userService = new UserServiceImpl();
+        int PEOPLE_COUNT = 0;
+        UserService userService = new UserServiceImpl();
         userService.createUsersTable();
-        userService.saveUser("Egor", "Simonov", (byte)23);
-        userService.saveUser("Oleg", "Olegov", (byte)26);
-        userService.saveUser("Maxim", "ivanov", (byte)32);
+        userService.saveUser("Oleg", "Olegov", (byte)33);
+        userService.saveUser("Olga", "Ermilova", (byte)26);
+        userService.saveUser("Maxim", "Ivanov", (byte)32);
         userService.saveUser("Stas", "Sidorov", (byte)40);
         System.out.println(userService.getAllUsers());
         userService.cleanUsersTable();
