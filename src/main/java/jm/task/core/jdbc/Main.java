90f6098b1cc1744d945dbab2ae4da013077f047e
Maxim Glebov <maglebov@gmail.com>
2024-11-19 16:39:59 +0300

    deleted not-use imports

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,13 +1,7 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
-import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
-
-import java.sql.Connection;
-import java.util.List;
 
 public class  Main {
     public static void main(String[] args) {
