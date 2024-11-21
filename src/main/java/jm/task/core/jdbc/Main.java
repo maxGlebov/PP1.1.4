134d926651651bf0a4b640ced0b71e58545e62e9
Maxim Glebov <maglebov@gmail.com>
2024-11-22 00:50:33 +0300

    edited

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,20 +1,25 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class  Main {
     public static void main(String[] args) {
-        int PEOPLE_COUNT = 0;
-        UserService userService = new UserServiceImpl();
-        userService.createUsersTable();
-        userService.saveUser("Oleg", "Olegov", (byte)33);
-        userService.saveUser("Olga", "Ermilova", (byte)26);
-        userService.saveUser("Maxim", "Ivanov", (byte)32);
-        userService.saveUser("Stas", "Sidorov", (byte)40);
-        System.out.println(userService.getAllUsers());
-        userService.cleanUsersTable();
-        userService.dropUsersTable();
+        User user1 = new User("Oleg", Ol)
+
+
+
+//        int PEOPLE_COUNT = 0;
+//        UserService userService = new UserServiceImpl();
+//        userService.createUsersTable();
+//        userService.saveUser("Oleg", "Olegov", (byte)33);
+//        userService.saveUser("Olga", "Ermilova", (byte)26);
+//        userService.saveUser("Maxim", "Ivanov", (byte)32);
+//        userService.saveUser("Stas", "Sidorov", (byte)40);
+//        System.out.println(userService.getAllUsers());
+//        userService.cleanUsersTable();
+//        userService.dropUsersTable();
 
 
 
