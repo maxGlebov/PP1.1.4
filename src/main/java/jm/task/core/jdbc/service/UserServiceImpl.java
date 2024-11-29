4612a2ef91869f863b57cbb3d173925f75755222
Maxim Glebov <maglebov@gmail.com>
2024-11-29 17:32:28 +0300

    edited service

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -8,41 +8,35 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
     public UserServiceImpl() {
-
+        static UserDao userDao = new UserDaoHibernateImpl();
     }
 
+
     public void createUsersTable() {
-        UserDao userDao = new UserDaoHibernateImpl();
+
         userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-        UserDao userDao = new UserDaoHibernateImpl();
         userDao.dropUsersTable();
-
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        UserDao userDao = new UserDaoHibernateImpl();
         userDao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        UserDao userDao = new UserDaoHibernateImpl();
         userDao.removeUserById(id);
 
     }
 
     public List<User> getAllUsers() {
-        UserDao userDao = new UserDaoHibernateImpl();
         List<User> list = new ArrayList<>();
         list = userDao.getAllUsers();
         return list;
     }
 
     public void cleanUsersTable() {
-        UserDao userDao = new UserDaoHibernateImpl();
         userDao.cleanUsersTable();
-
     }
 }
